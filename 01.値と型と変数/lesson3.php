16d250aadb449d6d903164e88321d93aafae4d0e
Masasshi Morioka <m.morioka.cingroup2012@gmail.com>
2024-03-20 03:03:51 +0900

    first commit

---
+++ 01.値と型と変数/lesson3.php
@@ -0,0 +1,16 @@
+﻿<?php
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+
+// 山田
+// 20歳・女性
+
+// という形で出力してください。
+
+$arr = ['name' => '山田','age' => '20','gender' => '女性'];
+
+echo $arr['name'];
+echo '<br>';
+echo $arr['age'].'・'.$arr['gender'];
